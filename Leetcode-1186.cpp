8613290efe745ff3e1de1740391cbd23c0f6dbc6
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2025-12-28 21:35:14 +0530

    28 December 2025

--- Leetcode-1186.cpp
+++ Leetcode-1186.cpp
@@ -2,16 +2,19 @@
  * Problem 1186: Maximum Subarray Sum with One Deletion
  * Language: C++
  */
-class Solution {
+class Solution {
 public:
-    int maximumSum(vector<int>& arr) {
-        int n = arr.size();
-        if(n == 1) return arr[0];
-        int take = max(arr[0], arr[1]);
-        int notTake = max(arr[0]+arr[1], arr[1]);
-        int ans = max(take, notTake);
-        for(int i = 2; i < arr.size(); i++){
-            take = max(notTake, arr[i]+take);
-            notTake = max(notTake+arr[i], arr[i]);
-            ans = max(ans, max(take, notTake));
-        }
+    int maximumSum(vector<int>& arr) {
+        int n = arr.size();
+        if(n == 1) return arr[0];
+        int take = max(arr[0], arr[1]);
+        int notTake = max(arr[0]+arr[1], arr[1]);
+        int ans = max(take, notTake);
+        for(int i = 2; i < arr.size(); i++){
+            take = max(notTake, arr[i]+take);
+            notTake = max(notTake+arr[i], arr[i]);
+            ans = max(ans, max(take, notTake));
+        }
+        return ans;
+    }
+};
