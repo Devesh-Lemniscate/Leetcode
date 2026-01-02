8d8708de95bd233b61cd4d34110b7ecb9bd18b4e
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-01-02 06:26:33 +0530

    POTD-02 January 2026

---
+++ Leetcode-961.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem 961: N-Repeated Element in Size 2N Array (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        for(int i = 0; i < nums.size()-2; i++)
+            if(nums[i] == nums[i+1] || nums[i] == nums[i+2]) 
+                return nums[i];
+        
+        return nums[nums.size()-1];
+    }
+};
