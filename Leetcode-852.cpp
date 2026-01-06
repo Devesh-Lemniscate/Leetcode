e4702a3a237120d5a55c0131c72f24dc47f1296d
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-01-06 18:46:08 +0530

    06 January 2026

--- Leetcode-852.cpp
+++ Leetcode-852.cpp
@@ -5,6 +5,15 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        
+        int n = arr.size(), l = 0, h = n-1;
+        while(l<=h){
+            int m = l+(h-l)/2;
+            if(m==0) return m+1;
+            if(arr[m-1] < arr[m] && arr[m+1]<arr[m]) return m;
+            else if(arr[m-1] < arr[m]) l = m+1;
+            else if(arr[m+1]<arr[m]) h = m-1;
+            m=l+(h-l)/2;
+        }
+        return -1;
     }
 };
