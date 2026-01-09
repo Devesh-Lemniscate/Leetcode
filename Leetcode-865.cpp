a890741d79cd042b18f3b7d22e9e81bbed2b0bff
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-01-09 07:43:23 +0530

    POTD-09 January 2026

---
+++ Leetcode-865.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem 865: Smallest Subtree with all the Deepest Nodes (POTD)
+ * Language: C++
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        
+    }
+};
