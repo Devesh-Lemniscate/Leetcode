773b7a637aa764e14f0a15f5a1ec0a6f32d6222a
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-01-10 09:39:42 +0530

    10 January 2026

---
+++ Leetcode-1448.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem 1448: Count Good Nodes in Binary Tree
+ * Language: C++
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        
+    }
+};
