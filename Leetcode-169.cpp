5a3697b5f612c3bb190528e16d52c88447f55906
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-01-13 09:27:18 +0530

    13 January 2026

--- Leetcode-169.cpp
+++ Leetcode-169.cpp
@@ -5,6 +5,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
+        int num = nums[0];
+        int len = nums.size();
+        int count = 0;
+        for(int i = 0; i < len; i++){
+            if(nums[i] != num && count) count--;
+            else if(nums[i] == num) count++;
+            else if(nums[i] != num && count == 0){
+                num = nums[i];
+                count++;
+            }
+        }
+        return num;
     }
 };
