ac211730e58beb72070461b3d2361c7c68f7e21a
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-01-13 09:15:45 +0530

    13 January 2026

--- Leetcode-189.cpp
+++ Leetcode-189.cpp
@@ -3,8 +3,30 @@
  * Language: C++
  */
 class Solution {
+
+private:
+    void rev(vector<int> &nums, int i, int j){
+        while(i < j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++; j--;
+        }
+    }
+
 public:
     void rotate(vector<int>& nums, int k) {
-        
+        int len = nums.size();
+        k %= len;
+        if(k==0) return;
+        reverse(nums.begin(), nums.end());
+        rev(nums, 0, k-1);
+        rev(nums, k, len-1);
     }
 };
+// 1 2 3 4 5 6 7
+// 7 6 5 4 3 2 1
+
+
+// -1 -100 3 99
+// 99 3 -100 -1
