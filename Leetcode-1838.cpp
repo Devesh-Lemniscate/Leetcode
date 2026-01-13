31e4a56465854a531117fbde2dd816a191d1ab06
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-01-13 09:22:01 +0530

    13 January 2026

--- Leetcode-1838.cpp
+++ Leetcode-1838.cpp
@@ -2,9 +2,20 @@
  * Problem 1838: Frequency of the Most Frequent Element
  * Language: C++
  */
+#define ll long long
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        
+        sort(nums.begin(),nums.end());
+        ll i=0, j = 0, sum = 0, maxLen = 0;
+        for (i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            while ((i - j + 1) * nums[i] - sum > k){
+                sum -= nums[j];
+                j++;
+            }
+            maxLen = max(maxLen, i-j+1);
+        }
+        return maxLen;
     }
 };
