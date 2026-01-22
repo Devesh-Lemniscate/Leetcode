fa92e34004340e554179830b9862bfdd3668b28f
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-01-22 05:38:35 +0530

    POTD-22 January 2026

---
+++ Leetcode-3507.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 3507: Minimum Pair Removal to Sort Array I (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    int minimumPairRemoval(vector<int>& nums) {
+        
+    }
+};
