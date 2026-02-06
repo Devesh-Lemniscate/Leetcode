fdce300dcd33041d863152ff8bb24f3550d46b59
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-02-06 05:39:51 +0530

    POTD-06 February 2026

--- Leetcode-3634.cpp
+++ Leetcode-3634.cpp
@@ -5,6 +5,17 @@
 class Solution {
 public:
     int minRemoval(vector<int>& nums, int k) {
-        
+        int len = nums.size();
+        int left = 0, right = 0;
+        int ans = INT_MAX;
+        sort(nums.begin(), nums.end());
+        while(left < len && right < len){
+            long long num = nums[left];
+            while(right < len && num * k >= (long long)nums[right]) right++;
+            ans = min(ans, len-right+left);
+            left++;
+        }
+        ans = min(ans, len-right+left);
+        return ans;
     }
 };
