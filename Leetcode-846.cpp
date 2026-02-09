6467989f22e3c0f3e7642f53035624179a6cc785
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-02-09 19:58:59 +0530

    09 February 2026

---
+++ Leetcode-846.cpp
@@ -0,0 +1,27 @@
+/*
+ * Problem 846: Hand of Straights
+ * Language: C++
+ */
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) return false;
+
+        unordered_map<int, int> count;
+        for (auto card : hand) count[card]++;
+        for (auto card : hand) {
+            int start = card;
+            while (count[start - 1] > 0) start--;
+            while (start <= card) {
+                while (count[start] > 0) {
+                    for (int nextCard = start; nextCard < start + groupSize; nextCard++) {
+                        if (count[nextCard] == 0) return false;
+                        count[nextCard]--;
+                    }
+                }
+                start++;
+            }
+        }
+        return true;
+    }
+};
