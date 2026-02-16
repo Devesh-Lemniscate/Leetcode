97a5c65392d74ee091d05e0ed94b9e1df5fb9d6e
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-02-16 05:34:35 +0530

    POTD-16 February 2026

--- Leetcode-190.cpp
+++ Leetcode-190.cpp
@@ -5,6 +5,13 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        
+        bitset<32> b(n);   
+        int size = b.size();
+        for (int i = 0; i < size/2; i++) {
+            bool tmp = b[i];
+            b[i] = b[size - 1 - i];
+            b[size - 1 - i] = tmp;
+        }
+        return (int)b.to_ulong();  
     }
 };
