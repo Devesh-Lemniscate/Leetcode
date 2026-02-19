8ae3a51dc263a9470664c85d6c2b322b1dd082c1
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-02-19 05:43:09 +0530

    POTD-19 February 2026

--- Leetcode-696.cpp
+++ Leetcode-696.cpp
@@ -5,6 +5,15 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        
+        int curr = 1, prev = 0, ans = 0;
+        for(int i = 1; i < s.size(); i++){
+            if(s[i-1] == s[i]) curr++;
+            else{
+                ans += min(curr, prev);
+                prev = curr;
+                curr = 1;
+            }
+        }
+        return ans+min(curr, prev);
     }
 };
