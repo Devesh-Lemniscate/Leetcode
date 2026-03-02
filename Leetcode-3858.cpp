a33fbc3816c166f7a34a36d5ad6225f628fdb650
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-02 22:19:39 +0530

    02 March 2026

---
+++ Leetcode-3858.cpp
@@ -0,0 +1,30 @@
+/*
+ * Problem 3858: Minimum Bitwise OR From Grid
+ * Language: C++
+ */
+class Solution {
+public:
+    int minimumOR(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        int ans = 0;
+        for(int i = 31; i >= 0; i--){
+            vector<vector<int>> val(n);
+            bool check = true;
+            for(int j = 0; j < n; j++){
+                //bool flag = false;
+                for(int k = 0; k < grid[j].size(); k++){
+                    if(!(grid[j][k] & (1 << i))){
+                        val[j].push_back(grid[j][k]);
+                    }
+                }
+                if(!val[j].size()){
+                    check = false;
+                    break;
+                }
+            }
+            if(!check) ans = ans | (1 << i);
+            else grid = val;
+        }
+        return ans;
+    }
+};
