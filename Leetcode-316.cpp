4360c6f3184e32ac67c8212733bc23ff35d7c17c
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-18 16:06:14 +0530

    18 March 2026

--- Leetcode-316.cpp
+++ Leetcode-316.cpp
@@ -5,49 +5,22 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int> firstSeen(27, INT_MAX), lastSeen(27, INT_MAX);
-        for(int index = 0; index < s.size(); index++){
-            if(firstSeen[s[index] - 'a'] == INT_MAX){
-                firstSeen[s[index]-'a'] = index;
-            }
-        }
+        vector<int> lastSeen(27, INT_MAX);
+        vector<bool> seen(26, false);
         for(int index = 0; index < s.size(); index++){
             lastSeen[s[index]-'a'] = index;
         }
-        
         string ans = "";
-        
-        for(int idx = 0; idx < 26; idx++){
-            int minLast = INT_MAX;
-            for(int i = 0; i < 26; i++) {
-                if(lastSeen[i] != INT_MAX) minLast = min(minLast, lastSeen[i]);
+        for(int i = 0; i < s.size(); i++){
+            int num = s[i] - 'a';
+            if(seen[num]) continue;
+            while(ans.size() && s[i] < ans[ans.size()-1] && lastSeen[ans[ans.size()-1]-'a'] > i){
+                seen[ans[ans.size()-1] - 'a'] = false;
+                ans.pop_back();
             }
-            if(minLast == INT_MAX) break; 
+            ans.push_back(num+'a');
+            seen[num] = true;
 
-            int currIdx = 26;
-            for(int index = 0; index < 26; index++){
-                if(lastSeen[index] != INT_MAX && firstSeen[index] <= minLast){
-                    currIdx = index;
-                    break;
-                }
-            }
-            
-            if(currIdx != 26){
-                ans.push_back(currIdx + 'a');
-                int pickedPos = firstSeen[currIdx]; 
-                
-                firstSeen[currIdx] = INT_MAX;
-                lastSeen[currIdx] = INT_MAX;
-                for(int i = 0; i < 26; i++){
-                    if(lastSeen[i] != INT_MAX && firstSeen[i] < pickedPos){
-                        int nextPos = pickedPos + 1;
-                        while(nextPos < s.size() && s[nextPos] - 'a' != i) {
-                            nextPos++;
-                        }
-                        firstSeen[i] = nextPos;
-                    }
-                }
-            } else break;
         }
         return ans;
     }
