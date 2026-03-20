5cfd12eae84f8e45c18452f3830dcc5afd0b9ebb
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-20 07:45:27 +0530

    POTD-20 March 2026

--- Leetcode-3567.cpp
+++ Leetcode-3567.cpp
@@ -5,6 +5,24 @@
 class Solution {
 public:
     vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
-        
+        int n = grid.size(), m= grid[0].size();
+        vector<vector<int>> ans(n-k+1, vector<int> (m-k+1));
+        for(int i = 0; i <n-k+1; i++){
+            for(int j = 0; j < m-k+1; j++){
+                vector<int> arr;
+                for(int l = 0; l < k; l++){
+                    for(int p = 0; p< k; p++){
+                        arr.push_back(grid[i+l][j+p]);
+                    }
+                }
+                sort(arr.begin(), arr.end());
+                int mn = INT_MAX;
+                for(int l = 1; l < arr.size(); l++){
+                    if(arr[l]!=arr[l-1]) mn = min(mn, abs(arr[l]-arr[l-1]));
+                }
+                ans[i][j] = (mn==INT_MAX?0:mn);
+            }
+        }
+        return ans;
     }
 };
