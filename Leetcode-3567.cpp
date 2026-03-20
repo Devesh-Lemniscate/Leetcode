efd50cf8b72973ef0053fcb5aa11768dba636d50
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-20 07:17:28 +0530

    POTD-20 March 2026

---
+++ Leetcode-3567.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 3567: Minimum Absolute Difference in Sliding Submatrix (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
+        
+    }
+};
