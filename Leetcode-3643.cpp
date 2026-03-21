fe094c58b3722ba334d394f57e38a33d51cf73f5
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-21 08:22:28 +0530

    POTD-21 March 2026

--- Leetcode-3643.cpp
+++ Leetcode-3643.cpp
@@ -5,6 +5,13 @@
 class Solution {
 public:
     vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
-        
+        int i = x;
+        int j = x+k-1;
+        while(i < j){
+            for(int it = y; it < y+k; it++) swap(grid[i][it], grid[j][it]);
+            i++;
+            j--;
+        }
+        return grid;
     }
 };
