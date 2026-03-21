30aa25fff1a5210455eb824e6721c5ffbbc0db65
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-21 08:21:40 +0530

    POTD-21 March 2026

---
+++ Leetcode-3643.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 3643: Flip Square Submatrix Vertically (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
+        
+    }
+};
