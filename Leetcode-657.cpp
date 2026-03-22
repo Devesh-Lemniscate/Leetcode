9d7b25f92209b41f26332e58302db0b810ddc5f1
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-23 05:14:42 +0530

    23 March 2026

---
+++ Leetcode-657.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem 657: Robot Return to Origin
+ * Language: C++
+ */
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for(auto &move: moves){
+            if(move == 'U') y++;
+            if(move == 'D') y--;
+            if(move == 'L') x--;
+            if(move == 'R') x++;
+        }
+        return (x==0 && y==0);
+    }
+};
