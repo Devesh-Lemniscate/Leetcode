a265ba08c87b38f390a1d24471a9a19b6f32ece3
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-26 10:34:33 +0530

    26 March 2026

--- Leetcode-5.cpp
+++ Leetcode-5.cpp
@@ -3,8 +3,31 @@
  * Language: C++
  */
 class Solution {
+private:
+    int helper(const string &s,vector<vector<int>> &dp, int i, int j){   
+        if(i==j || i>j) return 1;   
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s[i]==s[j]) dp[i][j]=helper(s,dp,i+1,j-1);
+        else dp[i][j]=0;
+        helper(s,dp,i+1,j);
+        helper(s,dp,i,j-1);
+        return dp[i][j];  
+    }
+    
 public:
     string longestPalindrome(string s) {
-        
+        int n=s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        helper(s,dp,0,n-1);
+        int n1=0,n2=0;
+        for(int i=0; i<=n; i++){
+            for(int j=i+1; j<=n; j++){
+                if(dp[i][j]==1 && j-i>n2-n1){
+                    n1=i;
+                    n2=j;
+                }
+            }
+        }    
+        return s.substr(n1,n2-n1+1);
     }
 };
