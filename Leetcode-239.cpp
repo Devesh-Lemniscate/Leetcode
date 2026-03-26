2946c8075551b32750eaac487139223fce0a960b
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-26 18:25:44 +0530

    26 March 2026

--- Leetcode-239.cpp
+++ Leetcode-239.cpp
@@ -5,6 +5,16 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
+        int len = nums.size();
+        deque<int> dq;
+        vector<int> answer;
+        for(int index = 0; index < len; index++){
+            while(!dq.empty() && nums[dq.back()] < nums[index]) dq.pop_back();
+            int validIndex = index-k+1;
+            while(!dq.empty() && dq.front() < validIndex) dq.pop_front();
+            dq.push_back(index);
+            if(index >= k-1) answer.push_back(nums[dq.front()]);
+        }
+        return answer;
     }
 };
