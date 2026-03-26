3aa2a66fe001438167504fc3a8f6dda622e3f7ab
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-26 06:21:39 +0530

    26 March 2026

--- Leetcode-560.cpp
+++ Leetcode-560.cpp
@@ -5,6 +5,16 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        
+        int n = nums.size(), cnt = 0;
+        if(n==1 && nums[0]==k) return 1;
+        if(n==1) return 0;
+        for(int i = 1; i < n; i++) nums[i] += nums[i-1];   
+        unordered_map<int, int> m;
+        for(int i = 0; i < n; i++){
+            if(nums[i]==k) cnt++;
+            if(m.find(nums[i]-k)!=m.end()) cnt+=m[nums[i]-k];
+            m[nums[i]]++;
+        }
+        return cnt;
     }
 };
