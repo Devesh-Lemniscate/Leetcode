f362fc9ef10ee9fafa2d18af09cde027aa74306e
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-26 13:43:14 +0530

    26 March 2026

---
+++ Leetcode-496.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 496: Next Greater Element I
+ * Language: C++
+ */
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
