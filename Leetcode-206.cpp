60965cb25bb4889ac35ce69d6f7986dc929aaf88
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-26 11:36:44 +0530

    26 March 2026

--- Leetcode-206.cpp
+++ Leetcode-206.cpp
@@ -15,6 +15,13 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        
+        ListNode *curr = head, *prev =NULL;
+        while(curr){
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 };
