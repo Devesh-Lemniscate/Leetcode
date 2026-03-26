24094b352f660aa5b246c42892a74616af51d364
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-26 21:36:27 +0530

    26 March 2026

--- Leetcode-94.cpp
+++ Leetcode-94.cpp
@@ -16,6 +16,26 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        
+        TreeNode *curr = root, *pred = NULL;
+        vector<int> ans;
+        while(curr){
+            if(curr->left){
+                pred = curr->left;
+                while(pred->right && pred->right != curr) pred = pred->right;
+                if(pred->right==NULL){ // link
+                    pred->right = curr;
+                    curr = curr->left;
+                }
+                else if(pred->right==curr){ // unlink
+                    pred->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }else{
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+        }
+        return ans;
     }
 };
