c85a85ff9e0ac2d3cdc24bf41e3b5dee7bb591ed
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-27 07:11:44 +0530

    27 March 2026

---
+++ Leetcode-1616.cpp
@@ -0,0 +1,32 @@
+/*
+ * Problem 1616: Split Two Strings to Make Palindrome
+ * Language: C++
+ */
+class Solution {
+private:
+    bool check(string a, string b){
+        int i = 0, j = b.size()-1;
+        int len = a.size();
+        while(a[i] == b[j] && i < j){
+            i++; j--;
+        }
+        int f1 = true, f2 = true;
+        int l = i, r = j;
+        while(l < r){
+            if(len - l*2 == 1) break;
+            if(b[l] != b[r]) f1 = false;
+            l++; r--;
+        }
+        l = i, r= j;
+        while(l < r){
+            if(len - l*2 == 1) break;
+            if(a[l] != a[r]) f2 = false;
+            l++; r--;
+        }
+        return f1 || f2;
+    }
+public:
+    bool checkPalindromeFormation(string a, string b) {
+        return check(a, b) || check(b,a);
+    }
+};
