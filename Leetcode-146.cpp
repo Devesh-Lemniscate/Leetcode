845fd72d1c3eb265ecd5072cdc6bb642df94d77f
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-03-29 16:06:05 +0530

    29 March 2026

---
+++ Leetcode-146.cpp
@@ -0,0 +1,25 @@
+/*
+ * Problem 146: LRU Cache
+ * Language: C++
+ */
+class LRUCache {
+public:
+    LRUCache(int capacity) {
+        
+    }
+    
+    int get(int key) {
+        
+    }
+    
+    void put(int key, int value) {
+        
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
