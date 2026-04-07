9da61ffd92589db37a26fd37affd63f9aec5a48c
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-07 17:58:21 +0530

    07 April 2026

--- Leetcode-26.cpp
+++ Leetcode-26.cpp
@@ -5,6 +5,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        int i = 0, j = 1;
+        int idx = 0, n = nums.size();
+        while(i < n && j < n){
+            while(j < n && nums[j] == nums[j-1]) j++;
+            if(j == n) break;
+            idx++;
+            nums[idx] = nums[j];
+            j++;
+        }
+        return idx+1;
     }
 };
