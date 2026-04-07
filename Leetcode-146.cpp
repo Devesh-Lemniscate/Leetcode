e62a74f834abe0b3915a22a954d3285777737207
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-07 21:52:29 +0530

    07 April 2026

--- Leetcode-146.cpp
+++ Leetcode-146.cpp
@@ -3,6 +3,7 @@
  * Language: C++
  */
 class Node{
+public:
     Node* prev, *next;
     int key, val;
     Node(int k, int v){
@@ -11,19 +12,60 @@ class Node{
         key = k;
         val = v;
     }
-}
+};
 class LRUCache {
 public:
+    int cap;
+    Node* head, *tail;
+    unordered_map<int, Node*> mp;
     LRUCache(int capacity) {
-        
+        cap = capacity;
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head->next = tail;
+        tail->prev = head;
     }
     
     int get(int key) {
-        
+        if(!mp.count(key)) return -1;
+        Node* temp = mp[key];
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+        temp->next = tail;
+        temp->prev = tail->prev;
+        tail->prev->next = temp;
+        tail->prev = temp;
+        return mp[key]->val;
     }
     
     void put(int key, int value) {
-        
+        if(mp.count(key)){
+            Node* temp = mp[key];
+            
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
+
+            temp->val = value;
+
+            temp->next = tail;
+            temp->prev = tail->prev;
+            tail->prev->next = temp;
+            tail->prev =temp;
+        }else{
+            if(mp.size() == cap){
+                mp.erase(head->next->key);
+                Node* temp = head->next;
+                head->next = head->next->next;
+                head->next->prev = head;
+                delete temp;
+            }
+            Node *temp = new Node(key, value);
+            temp->next = tail;
+            temp->prev = tail->prev;
+            tail->prev->next = temp;
+            tail->prev =temp;
+            mp[key] = temp;
+        }
     }
 };
 
