3d1b0986f07b58e28d4bb62ede902e7677b9b2da
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-08 07:58:32 +0530

    08 April 2026

--- Leetcode-17.cpp
+++ Leetcode-17.cpp
@@ -3,8 +3,29 @@
  * Language: C++
  */
 class Solution {
+private:
+    void findCombinations(int index, string &digits, unordered_map<char, string> &mp, vector<string> &ans, string current){
+        if(current.size() == digits.size()){
+            ans.push_back(current);
+            return;
+        }
+        for(auto digit: mp[digits[index]]){
+            findCombinations(index+1, digits, mp, ans, current + digit);
+        }
+    }
 public:
     vector<string> letterCombinations(string digits) {
-        
+        unordered_map<char, string> mp;
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+        vector<string> ans;
+        findCombinations(0, digits, mp, ans, "");
+        return ans;
     }
 };
