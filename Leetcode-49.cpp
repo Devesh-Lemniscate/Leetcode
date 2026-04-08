b04e26375d7d61a07ae3e80d28c4098b0107f6be
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-08 07:01:40 +0530

    08 April 2026

--- Leetcode-49.cpp
+++ Leetcode-49.cpp
@@ -5,6 +5,14 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
+        unordered_map<string, vector<string>> mp;
+        for(auto str: strs){
+            string org = str;
+            sort(str.begin(), str.end());
+            mp[str].push_back(org);
+        }
+        vector<vector<string>> ans;
+        for(auto it: mp) ans.push_back(it.second);
+        return ans;
     }
 };
