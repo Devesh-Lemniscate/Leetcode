32ce8ac9a5561f4564b0f8e1a204d6153f53756f
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-08 14:30:27 +0530

    08 April 2026

--- Leetcode-992.cpp
+++ Leetcode-992.cpp
@@ -3,8 +3,26 @@
  * Language: C++
  */
 class Solution {
+private:
+    int helper(vector<int> &nums, int k){
+        int left = 0, right = 0;
+        int len = nums.size();
+        unordered_map<int, int> mp;
+        int count = 0;
+        while(right < len){
+            mp[nums[right]]++;
+            while(mp.size() > k){
+                mp[nums[left]]--;
+                if(mp[nums[left]] == 0) mp.erase(nums[left]);
+                left++;
+            }
+            count += right-left+1;
+            right++;
+        }
+        return count;
+    }
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        
+        return helper(nums, k) - helper(nums, k-1);
     }
 };
