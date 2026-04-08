8a9492531eca5bacffa4dd74317b29d0b779afe9
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-08 14:25:33 +0530

    08 April 2026

---
+++ Leetcode-992.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 992: Subarrays with K Different Integers
+ * Language: C++
+ */
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        
+    }
+};
