170408b1d7ee4363d16892ea93448e86d8ce70ea
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-08 06:01:30 +0530

    08 April 2026

--- Leetcode-3.cpp
+++ Leetcode-3.cpp
@@ -1,16 +1,30 @@
+/*
+ * Problem 3: Longest Substring Without Repeating Characters
+ * Language: C++
+ */
 class Solution {
-    public:
-        int lengthOfLongestSubstring(string s) {
-            int arr[256] = {0};
-            int ans = 0;
-            for(int i = 0, j = 0; i < s.size(); i++){
-                arr[(int)(s[i])]++;
-                while(arr[s[i]] > 1){
-                    arr[s[j]]--;
-                    j++;
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> mp;
+        int left = 0, right = 0;
+        int len = s.size();
+        int maxi = 0;
+        while(left < len && right < len){
+            if(mp.count(s[right])){
+                while(s[left] != s[right]){
+                    mp[s[left]]--;
+                    if(mp[s[left]] == 0) mp.erase(s[left]);
+                    left++;
                 }
-                ans = max(ans, i-j+1);
+                mp[s[left]]--;
+                if(mp[s[left]] == 0) mp.erase(s[left]);
+                left++;
             }
-            return ans;
+            maxi = max(maxi, right-left+1);
+            mp[s[right]]++;
+            right++;
         }
-    };
+        maxi = max(maxi, right-left);
+        return maxi;
+    }
+};
