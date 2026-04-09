bdc4a52059c50eaad94774db653361c1184e2567
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-09 20:54:28 +0530

    09 April 2026

--- Leetcode-337.cpp
+++ Leetcode-337.cpp
@@ -14,8 +14,18 @@
  * };
  */
 class Solution {
+private:
+    int helper(TreeNode* root, unordered_map<TreeNode*, int> &dp){
+        if(!root) return 0;
+        if(dp.count(root)) return dp[root];
+        int ans = 0;
+        if(root->left) ans += helper(root->left->right, dp) + helper(root->left->left, dp);
+        if(root->right) ans += helper(root->right->right, dp) + helper(root->right->left, dp);
+        return dp[root] = max(ans + root->val, helper(root->right, dp) + helper(root->left, dp));
+    }
 public:
     int rob(TreeNode* root) {
-        
+        unordered_map<TreeNode*, int> dp;
+        return helper(root, dp);
     }
 };
