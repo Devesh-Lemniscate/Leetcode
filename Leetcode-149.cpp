76bfdd4a2bdcd471388eab8c8802661fc027f0a8
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-09 05:49:27 +0530

    09 April 2026

--- Leetcode-149.cpp
+++ Leetcode-149.cpp
@@ -5,6 +5,27 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        
+        int n = points.size();
+        int maxi = 0;
+        for(int i = 0; i < n; i++){
+            map<pair<int, int>, int> mp;
+            for(int j = 0; j < n; j++){
+                if(i == j) continue;
+                int num = points[i][0] - points[j][0];
+                int den = points[i][1] - points[j][1];
+                int com = gcd(num, den);
+                if(com != 0){
+                    num /= com;
+                    den /= com;
+                }
+                if(den < 0){
+                    num = -num;
+                    den = -den;
+                }
+                mp[{num, den}]++;
+                maxi = max(maxi, mp[{num, den}]);
+            }
+        }
+        return maxi+1;
     }
 };
