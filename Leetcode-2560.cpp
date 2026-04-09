116001a604ee09acdfa578016c0db78f6a698848
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-09 20:58:06 +0530

    09 April 2026

--- Leetcode-2560.cpp
+++ Leetcode-2560.cpp
@@ -1,30 +1,34 @@
+/*
+ * Problem 2560: House Robber IV
+ * Language: C++
+ */
 class Solution {
-    public:
-        bool check(vector<int> &nums, int k, int mid, int n){
-            for(int i = 0; i < n; i++){
-                if(nums[i] <= mid ){
-                    k--;i++;
-                }
-                if(k==0)return true;
+public:
+    bool check(vector<int> &nums, int k, int mid, int n){
+        for(int i = 0; i < n; i++){
+            if(nums[i] <= mid ){
+                k--;i++;
             }
-            return false;
+            if(k==0)return true;
         }
-        int minCapability(vector<int>& nums, int k) {
-            int n = nums.size();
-            int low = INT_MAX, high = INT_MIN;
-            for(int i = 0; i < n; i++){
-                low = min(low, nums[i]);
-                high = max(high, nums[i]);
-            }
-            int ans = 0;
-            while(low <= high){
-                int mid = low + (high-low)/2;
-                if(check(nums, k, mid, n)){
-                    ans = mid;
-                    high = mid-1;    
-                }
-                else low = mid+1;
-            }
-            return ans;
+        return false;
+    }
+    int minCapability(vector<int>& nums, int k) {
+        int n = nums.size();
+        int low = INT_MAX, high = INT_MIN;
+        for(int i = 0; i < n; i++){
+            low = min(low, nums[i]);
+            high = max(high, nums[i]);
         }
-    };
+        int ans = 0;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(check(nums, k, mid, n)){
+                ans = mid;
+                high = mid-1;    
+            }
+            else low = mid+1;
+        }
+        return ans;
+    }
+};
