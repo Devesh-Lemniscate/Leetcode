f2585727d1b622befadec30955e1b481940502f3
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-11 07:33:02 +0530

    POTD-11 April 2026

--- Leetcode-518.cpp
+++ Leetcode-518.cpp
@@ -1,10 +1,22 @@
 /*
- * Problem 518: Coin Change II
+ * Problem 518: Coin Change II (POTD)
  * Language: C++
  */
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
+    int helper(int amt, int idx, vector<int>& coins, vector<vector<int>> &dp){
+        if(amt==0) return 1;
+        if(idx >= coins.size() || amt < 0) return 0;
+        if(dp[idx][amt] != -1) return dp[idx][amt];
         
+        int take = helper(amt - coins[idx], idx, coins, dp);
+        int notTake = helper(amt, idx + 1, coins, dp);
+
+        return dp[idx][amt] = take + notTake;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
+        return helper(amount, 0, coins, dp);
     }
 };
