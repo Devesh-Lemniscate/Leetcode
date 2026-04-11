5304811bc7a2584c2b992e6fcaae8729d89a6b40
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-11 07:05:18 +0530

    POTD-11 April 2026

---
+++ Leetcode-3741.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem 3741: Minimum Distance Between Three Equal Elements II (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        unordered_map<int, queue<int>> mp;
+        int mini = INT_MAX;
+        for(int i = 0; i < nums.size(); i++){
+            if(mp[nums[i]].size() == 2){
+                int curr = mp[nums[i]].front(); mp[nums[i]].pop();
+                int dis = abs(i-curr) + abs(curr-mp[nums[i]].front()) + abs(i-mp[nums[i]].front());
+                mini = min(mini, dis);
+                mp[nums[i]].push(i);
+            }else mp[nums[i]].push(i);
+        }
+        return mini == INT_MAX ? -1 : mini;
+    }
+};
