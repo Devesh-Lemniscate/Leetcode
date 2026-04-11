f917e13d2f7201761637f0a9bafa9cb1af1057a1
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-11 06:17:16 +0530

    11 April 2026

--- Leetcode-151.cpp
+++ Leetcode-151.cpp
@@ -5,6 +5,23 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        
+        reverse(s.begin(), s.end());
+        int len = s.size();
+        int left = 0, right = 0;
+        int index = 0;
+        while(index < len){
+            while(index < len && s[index] == ' ') index++;
+            if(index == len) break;
+            while(index < len && s[index] != ' ') s[right++] = s[index++];
+            reverse(s.begin() + left, s.begin()+right);
+            s[right++] = ' ';
+            left = right;
+            index++;
+        }
+        s.resize(right-1);
+        return s;
     }
 };
+// "  dlrow olleh  "
+
+// "world "
