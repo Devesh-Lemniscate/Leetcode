6de95446c99481230b6a085282157136f1f7b10d
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-12 14:43:45 +0530

    12 April 2026

--- Leetcode-42.cpp
+++ Leetcode-42.cpp
@@ -5,6 +5,21 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        
+        int len = height.size();
+        int lmax = 0, rmax = 0;
+        int left = 0, right = len-1;
+        int sum = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                lmax = max(lmax, height[left]);
+                sum += lmax - height[left];
+                left++;
+            }else{
+                rmax = max(rmax, height[right]);
+                sum += rmax - height[right];
+                right--;
+            }
+        }
+        return sum;
     }
 };
