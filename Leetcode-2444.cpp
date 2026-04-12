dfed595d09d862327e6635e55c9025cb63117d75
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-13 05:06:50 +0530

    13 April 2026

---
+++ Leetcode-2444.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 2444: Count Subarrays With Fixed Bounds
+ * Language: C++
+ */
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        
+    }
+};
