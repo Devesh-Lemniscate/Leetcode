4a1e136e098229e2cfeb9122550631c96d53acc7
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-12 12:47:42 +0530

    12 April 2026

---
+++ Leetcode-295.cpp
@@ -0,0 +1,25 @@
+/*
+ * Problem 295: Find Median from Data Stream
+ * Language: C++
+ */
+class MedianFinder {
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        
+    }
+    
+    double findMedian() {
+        
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
