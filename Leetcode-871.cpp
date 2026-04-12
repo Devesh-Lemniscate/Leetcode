6e6c689a68ab6a8573d9c0435fb1c7cb84c1c127
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-13 04:28:06 +0530

    13 April 2026

---
+++ Leetcode-871.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 871: Minimum Number of Refueling Stops
+ * Language: C++
+ */
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        
+    }
+};
