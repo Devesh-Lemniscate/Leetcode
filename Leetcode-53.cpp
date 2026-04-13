63a8c982c70d764ad02e077c693794f771a8ca7e
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-13 05:42:35 +0530

    13 April 2026

--- Leetcode-53.cpp
+++ Leetcode-53.cpp
@@ -3,14 +3,30 @@
  * Language: C++
  */
 class Solution {
+private:
+    int help(int left, int mid, int right, vector<int> &nums){
+        int leftSum = INT_MIN, sum = 0;
+        for(int i = mid; i >= left; i--){
+            sum += nums[i];
+            leftSum = max(leftSum, sum);
+        }
+        int rightSum = INT_MIN; sum = 0;
+        for(int i = mid+1; i <= right; i++){
+            sum += nums[i];
+            rightSum = max(rightSum, sum);
+        }
+        return leftSum + rightSum;
+    }
+    int helper(int left, int right, vector<int> &nums){
+        if(left == right) return nums[left];
+        int mid = left + (right-left)/2;
+        int leftSum = helper(left, mid, nums);
+        int rightSum = helper(mid+1, right, nums);
+        int crossSum = help(left, mid, right, nums);
+        return max({leftSum, rightSum, crossSum});
+    }
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxi = INT_MIN, curr = 0;
-        for(auto it: nums){
-            if(curr < 0) curr = 0;
-            curr += it;
-            maxi = max(maxi, curr);
-        }
-        return maxi;
+        return helper(0, nums.size()-1, nums);
     }
 };
