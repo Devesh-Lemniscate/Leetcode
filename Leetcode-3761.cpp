10901ef28ea3fc7a7ed782b2b1b562541348f662
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-17 06:43:14 +0530

    POTD-17 April 2026

--- Leetcode-3761.cpp
+++ Leetcode-3761.cpp
@@ -4,7 +4,25 @@
  */
 class Solution {
 public:
-    int minMirrorPairDistance(vector<int>& nums) {
-        
+    int rev(int x){
+        int ans = 0;
+        while(x){
+            ans = ans * 10 + x % 10;
+            x /= 10;
+        }
+        return ans;
+    }
+
+    int minMirrorPairDistance(vector<int>& nums){
+        unordered_map<int, int> mp;
+        mp[rev(nums[0])] = 0;
+        int ans = INT_MAX;
+        for(int j = 1; j < nums.size(); j++){
+            if(mp.count(nums[j])){
+                ans = min(ans, j - mp[nums[j]]);
+            }
+            mp[rev(nums[j])] = j;
+        }
+        return (ans == INT_MAX) ? -1 : ans;
     }
 };
