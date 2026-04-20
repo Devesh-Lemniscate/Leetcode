26dbf93f6297526eaddb60b5873d857597e59448
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-20 07:41:56 +0530

    POTD-20 April 2026

--- Leetcode-2078.cpp
+++ Leetcode-2078.cpp
@@ -5,6 +5,9 @@
 class Solution {
 public:
     int maxDistance(vector<int>& colors) {
-        
+        int maxi = 0, len = colors.size();
+        for(int i = 1; i < len; i++) if(colors[i] != colors[0]) maxi = max(maxi, i);
+        for(int i = len-2; i >= 0; i--) if(colors[i] != colors[len-1]) maxi = max(maxi, len-1-i);
+        return maxi; 
     }
 };
