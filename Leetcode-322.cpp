a630c7954f4ab8fd77c89404c9cabf981755bcc3
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-20 10:10:26 +0530

    20 April 2026

--- Leetcode-322.cpp
+++ Leetcode-322.cpp
@@ -3,22 +3,26 @@
  * Language: C++
  */
 class Solution {
-private:
-    int helper(int idx, int amt, vector<int> & coins, vector<vector<int>> &dp){
-        if(idx >= coins.size() || amt < 0) return 1e9;
-        if(amt == 0) return 0;
-        if(dp[idx][amt] != -1) return dp[idx][amt];
-        int remain = 1 + helper(idx, amt-coins[idx], coins, dp);
-        int move = 1 + helper(idx+1, amt-coins[idx], coins, dp);
-        int notTake = helper(idx+1, amt, coins, dp);
-        return dp[idx][amt] = min({remain, move, notTake});
-    }
 public:
+    long long helper(vector<int>& coins, vector<int> &dp, int amt){
+        if(amt==0){
+            return 0;
+        }
+        if(dp[amt]!=-1)return dp[amt];
+        long long ans = INT_MAX;
+        for(int i = 0; i < coins.size(); i++){
+            if(amt-coins[i] < 0) continue;
+            ans = min(ans, (helper(coins, dp, amt-coins[i])+1));
+        }
+        return dp[amt] = ans;
+    }
     int coinChange(vector<int>& coins, int amount) {
+        if(amount ==0 )return 0;
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
-        int ans = helper(0, amount, coins, dp);
-        if(ans >= 1e9) return -1;
-        else return ans;
+        vector<int> dp(amount+1, -1);
+        helper(coins, dp, amount);
+        
+        if(dp[amount]==INT_MAX) return -1;
+        return dp[amount];
     }
 };
