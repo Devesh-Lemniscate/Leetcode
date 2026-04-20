84906ddf7fbad878c8057d3e4212259ddf3904a6
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-20 07:40:33 +0530

    POTD-20 April 2026

---
+++ Leetcode-2078.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 2078: Two Furthest Houses With Different Colors (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        
+    }
+};
