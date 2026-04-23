636d5332610da95c15204d9b4f7d273bb06ae2b2
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-23 11:12:23 +0530

    23 April 2026

---
+++ Leetcode-127.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 127: Word Ladder
+ * Language: C++
+ */
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+    }
+};
