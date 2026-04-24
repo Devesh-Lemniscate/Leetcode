0eb974b6e62f5c966d70a4c71e2902a876194615
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-24 10:37:29 +0530

    24 April 2026

---
+++ Leetcode-815.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 815: Bus Routes
+ * Language: C++
+ */
+class Solution {
+public:
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        
+    }
+};
