403717b252c4be8a8ba0d5b26e8967d9ea71fc9d
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-24 16:21:32 +0530

    24 April 2026

--- Leetcode-139.cpp
+++ Leetcode-139.cpp
@@ -4,11 +4,13 @@
  */
 class Solution {
 private:    
+    int dp[301];
     bool helper(int idx, string &s, vector<string> & words){
         int len = s.size();
         if(idx == len) return true;
+        if(dp[idx] != -1) return dp[idx];
         for(int i = 0; i < words.size(); i++){
-            string curr = words[0];
+            string curr = words[i];
             if(curr.size() > len-idx) continue;
             int prev = idx;
             bool flag = true;
@@ -20,12 +22,13 @@ private:
             }
             if(!flag) idx = prev;
             if(idx == len) return true;
-            if(helper(idx, s, words)) return dp[idx] = true;
+            if(flag && helper(idx, s, words)) return dp[idx] = true;
         }
         return dp[idx] = false;
     }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        memset(dp, -1, sizeof(dp));
         return helper(0, s, wordDict);
     }
 };
