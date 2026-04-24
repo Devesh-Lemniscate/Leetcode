351f0cbdd21ce358e6865f3bb104f0e6898a6b74
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-24 16:23:43 +0530

    24 April 2026

--- Leetcode-139.cpp
+++ Leetcode-139.cpp
@@ -12,17 +12,14 @@ private:
         for(int i = 0; i < words.size(); i++){
             string curr = words[i];
             if(curr.size() > len-idx) continue;
-            int prev = idx;
             bool flag = true;
             for(int j = 0; j < curr.size(); j++){
-                if(curr[j] != s[idx]){
+                if(curr[j] != s[idx+j]){
                     flag = false;
                     break;
-                }else idx++;
+                }
             }
-            if(!flag) idx = prev;
-            if(idx == len) return true;
-            if(flag && helper(idx, s, words)) return dp[idx] = true;
+            if(flag && helper(idx+curr.size(), s, words)) return dp[idx] = true;
         }
         return dp[idx] = false;
     }
