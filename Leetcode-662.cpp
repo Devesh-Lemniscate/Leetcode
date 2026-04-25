3ce23f3fa9929e8cbeeccb00fd4fbbf20c944b0f
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-25 10:16:11 +0530

    25 April 2026

---
+++ Leetcode-662.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem 662: Maximum Width of Binary Tree
+ * Language: C++
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        
+    }
+};
