3d87ffb6f5643f66e29dbb35748d2dd112f35a68
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-25 08:16:11 +0530

    25 April 2026

---
+++ Leetcode-901.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem 901: Online Stock Span
+ * Language: C++
+ */
+class StockSpanner {
+private:
+    stack<pair<int, int>> st;
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int ans = 1;
+        while(st.size() && st.top().first <= price){
+            ans += st.top().second;
+            st.pop();
+        }
+        st.push({price, ans});
+        return ans;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
