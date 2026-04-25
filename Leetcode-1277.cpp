dbb7f1ea2ab240882a8e88f25e4e20b800356469
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-25 11:05:45 +0530

    25 April 2026

---
+++ Leetcode-1277.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 1277: Count Square Submatrices with All Ones
+ * Language: C++
+ */
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        
+    }
+};
