12ee91659215e592d9e1a446359e727114d53638
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-29 17:45:52 +0530

    29 April 2026

--- Leetcode-2246.cpp
+++ Leetcode-2246.cpp
@@ -3,6 +3,16 @@
  * Language: C++
  */
 class Solution {
+private:
+    void dfs(int node, int par, vector<vector<int>> &adj){
+        int temp = 0;
+        for(auto it: adj[node]){
+            if(it != par && s[it] != s[node]){
+                temp++;
+                dfs(it, node, adj);
+            }
+        }
+    }
 public:
     int longestPath(vector<int>& parent, string s) {
         int n = parent.size();
@@ -11,6 +21,6 @@ public:
             adj[i].push_back(parent[i]);
             adj[parent[i]].push_back(i);
         }
-        
+        dfs(adj);        
     }
 };
