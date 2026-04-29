a82c0fc354e8bb0e987b0c397bbc596ffffe97d3
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-29 19:52:41 +0530

    29 April 2026

--- Leetcode-2246.cpp
+++ Leetcode-2246.cpp
@@ -4,18 +4,16 @@
  */
 class Solution {
 private:
-    int dfs(int node, int par, vector<vector<int>> &adj, int &ans, string& s){
+    int dfs(int node, int par, vector<vector<int>> &adj, int &ans, string &s){
         int temp1 = 0, temp2 = 0;
         for(auto it: adj[node]){
             if(it != par){
                 int child = dfs(it, node, adj, ans, s);
-                if(s[it] != s[node]){
-                    if(child > temp1){
-                        temp2 = temp1;
-                        temp1 = child;
-                    }else if(child > temp2){
-                        temp2 = child;
-                    }
+                if(child > temp1){
+                    temp2 = temp1;
+                    temp1 = child;
+                }else if(child > temp2){
+                    temp2 = child;
                 }
             }
         }
@@ -24,14 +22,14 @@ private:
     }
 public:
     int longestPath(vector<int>& parent, string s) {
-        int n = parent.size();
+        int n = s.size();
         vector<vector<int>> adj(n);
         for(int i = 1; i < n; i++){
             adj[i].push_back(parent[i]);
             adj[parent[i]].push_back(i);
         }
         int ans = 1;
-        dfs(0, -1, adj, ans, s);        
+        dfs(0, -1, adj, ans, s);
         return ans;
     }
 };
