7ae470695fcb23440cdb8919adacf9041fb62595
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-29 17:40:24 +0530

    29 April 2026

--- Leetcode-2246.cpp
+++ Leetcode-2246.cpp
@@ -4,28 +4,12 @@
  */
 class Solution {
 public:
-    vector<list<int>> adj;
-    string s;
-    int ans = 0;
-    int helper(int curr) {
-        int a = 0, b = 0;
-        for (auto it : adj[curr]) {
-            int len = helper(it);
-            if (s[it] == s[curr]) continue;
-            if (len >= a) {
-                b = a;
-                a = len;
-            } else if (len > b) b = len;
+    int longestPath(vector<int>& parent, string s) {
+        int n = parent.size();
+        vector<vector<pair<int, char>>> adj(n);
+        for(int i = 1; i < n; i++){
+            adj[i].push_back(parent[i]);
+            adj[parent[i]].push_back(i);
         }
-        ans = max(ans, a + b + 1);
-        return 1 + a;
-    }
-    int longestPath(vector<int>& parent, string t) {
-        adj.resize(parent.size());
-        for (int i = 1; i < parent.size(); i++) adj[parent[i]].push_back(i);
-        s = t;
-        ans = 1;
-        helper(0);
-        return ans;
     }
 };
