508a0abeda7fd47c38991ac8a752387b7e9fb3ce
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-04-30 07:49:53 +0530

    30 April 2026

--- Leetcode-32.cpp
+++ Leetcode-32.cpp
@@ -7,11 +7,15 @@ public:
     int longestValidParentheses(string s) {
         stack<int> st;
         st.push(-1);
+        int maxi = 0;
         for(int i  = 0; i < s.size(); i++){
             if(s[i] == '(') st.push(i);
             else{
-                if(st.size()) st.p
+                if(st.size()) st.pop();
+                if(st.empty()) st.push(i);
+                else maxi = max(maxi, i-st.top());
             }
         }
+        return maxi;
     }
 };
