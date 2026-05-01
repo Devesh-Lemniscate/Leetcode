e02fdde083cff24cb1fe49b70f3dc985958e70d2
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-01 21:54:21 +0530

    01 May 2026

---
+++ Leetcode-701.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem 701: Insert into a Binary Search Tree
+ * Language: C++
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        
+    }
+};
