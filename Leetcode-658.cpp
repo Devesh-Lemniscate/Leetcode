6fe8c8a308331621bf343ee9630a7ee1947b9ebb
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-01 21:39:14 +0530

    01 May 2026

---
+++ Leetcode-658.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 658: Find K Closest Elements
+ * Language: C++
+ */
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        
+    }
+};
