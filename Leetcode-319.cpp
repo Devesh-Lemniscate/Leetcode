f540bcefacc0d4f3864798b38e4efff27e5d8988
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-02 18:49:56 +0530

    02 May 2026

--- Leetcode-319.cpp
+++ Leetcode-319.cpp
@@ -5,6 +5,15 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        
+        int low = 0, high = n;
+        int on = 0;
+        while(low <= high){
+            long long mid = ((high - low) >> 1) + low;
+            if(mid * mid <= n){
+                on = mid;
+                low = mid+1;
+            }else high = mid-1;
+        }
+        return on;
     }
 };
