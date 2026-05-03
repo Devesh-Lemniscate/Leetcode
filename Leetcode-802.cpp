36ed0a18e8767df388fa924a1836aae8fc02d5bb
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-03 17:36:44 +0530

    03 May 2026

---
+++ Leetcode-802.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 802: Find Eventual Safe States
+ * Language: C++
+ */
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
+    }
+};
