8f7e38331d727b29204f9410a7ab8ca11b6e7cac
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-03 05:50:10 +0530

    POTD-03 May 2026

--- Leetcode-796.cpp
+++ Leetcode-796.cpp
@@ -2,9 +2,12 @@
  * Problem 796: Rotate String (POTD)
  * Language: C++
  */
+
 class Solution {
-public:
-    bool rotateString(string s, string goal) {
-        
-    }
-};
+    public:
+        bool rotateString(string& s, string& goal) {
+                return s.size()==goal.size() && (s+s).find(goal)!=-1;
+                    }
+                    };
+1
+
