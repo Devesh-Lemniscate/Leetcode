d676561f0ff335343f50ebd7fa192a033e17430a
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-04 21:06:57 +0530

    POTD-04 May 2026

--- Leetcode-48.cpp
+++ Leetcode-48.cpp
@@ -5,6 +5,17 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        
+        int n = matrix.size(), m = matrix[0].size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i>=j) continue;
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i = 0; i < m/2; i++){
+            for(int j = 0; j < n; j++){
+                swap(matrix[j][i], matrix[j][m-i-1]);
+            }
+        }
     }
 };
