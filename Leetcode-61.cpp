d3a449e61e93c5e3ee7e75641b8c9a430aa2a36e
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-05 14:44:17 +0530

    POTD-05 May 2026

--- Leetcode-61.cpp
+++ Leetcode-61.cpp
@@ -15,6 +15,27 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        
+        if(!head || !k) return head;
+        ListNode* first = head, *second = head, *prev = NULL;
+        int count = 0;
+        while(first){
+            first = first->next;
+            count++;
+        }
+        k = k % count;
+        if(!k) return head;
+        first = head;
+        while(k-- && first){
+            first = first->next;
+        }
+        while(first && first->next){
+            prev = first;
+            first = first->next;
+            second = second->next;
+        }
+        ListNode* newHead = second->next;
+        second->next = NULL;
+        if(first)first->next = head;
+        return newHead;
     }
 };
