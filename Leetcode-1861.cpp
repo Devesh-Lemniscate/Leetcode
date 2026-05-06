06a6506967fc71b32521aff7f8fa163343f58e63
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-06 06:30:33 +0530

    POTD-06 May 2026

--- Leetcode-1861.cpp
+++ Leetcode-1861.cpp
@@ -5,6 +5,27 @@
 class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
-        
+        int n = boxGrid.size(), m = boxGrid[0].size();
+        for(int i = 0; i < n; i++){
+            int j=0, count= 0;
+            while(j < m){
+                int k;
+                for (k = j; k < m; k++) {
+                    if (boxGrid[i][k] == '#') count++;
+                    if (boxGrid[i][k] == '*') break;
+                }
+                for (int l = j; l < k; l++) boxGrid[i][l] = '.';
+                for (int l = 1; l <= count; l++) boxGrid[i][k-l] = '#';
+                j = k + 1;
+                count = 0;
+            }
+        }
+        vector<vector<char>> res(m, vector<char>(n));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                res[j][n - 1 - i] = boxGrid[i][j];
+            }
+        }
+        return res;
     }
 };
