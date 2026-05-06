534e4bbee8cf69aaa33e005a628afbe28c9a39f9
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-06 06:28:09 +0530

    POTD-06 May 2026

---
+++ Leetcode-1861.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 1861: Rotating the Box (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        
+    }
+};
