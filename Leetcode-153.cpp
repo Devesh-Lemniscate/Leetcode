1d6119766d6d0c1773b2ad13a96cf612f5435d64
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-15 06:39:23 +0530

    POTD-15 May 2026

--- Leetcode-153.cpp
+++ Leetcode-153.cpp
@@ -5,6 +5,13 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        
+        int n = nums.size(), l = 0, r = n-1;
+        int m = 0;
+        while(l<r){
+            m = l + (r-l)/2;
+            if( nums[r] > nums[m]) r=m;
+            else if(nums[r] < nums[m]) l = m+1;
+        } 
+        return nums[l];
     }
 };
