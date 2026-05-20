9df5994512375b10baeb29aac29a360f1d958dbb
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-20 05:50:58 +0530

    POTD-20 May 2026

---
+++ Leetcode-2657.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 2657: Find the Prefix Common Array of Two Arrays (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        unordered_set<int> 
+    }
+};
