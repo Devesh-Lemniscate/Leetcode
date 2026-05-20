8628bdb8755865c4609c6ca62e624862590a41eb
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-20 05:52:48 +0530

    POTD-20 May 2026

--- Leetcode-2657.cpp
+++ Leetcode-2657.cpp
@@ -5,6 +5,17 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        unordered_set<int> 
+        int n = A.size(), cnt = 0;
+        vector<int> ans(n,0), mp(n+1, 0), tp(n+1, 0);
+        for(int i = 1; i < n+1; i++){
+            mp[A[i-1]]++;
+            tp[B[i-1]]++;
+            for(int j = 0; j < n; j++){
+                if(mp[j+1] && tp[j+1]) cnt++;
+            }
+            ans[i-1] = cnt;
+            cnt = 0;
+        }
+        return ans;
     }
 };
