2ce7ba974c1562f9fa4bb96f671c5f674fd981e8
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-05-30 07:28:44 +0530

    POTD-30 May 2026

---
+++ Leetcode-3161.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 3161: Block Placement Queries (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    vector<bool> getResults(vector<vector<int>>& queries) {
+        
+    }
+};
