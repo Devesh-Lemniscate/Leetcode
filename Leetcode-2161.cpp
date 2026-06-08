facb33a8f55731fe3435d8eabdd4e2cfd8309032
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-06-08 08:21:53 +0530

    POTD-08 June 2026

--- Leetcode-2161.cpp
+++ Leetcode-2161.cpp
@@ -1,16 +1,20 @@
+/*
+ * Problem 2161: Partition Array According to Given Pivot (POTD)
+ * Language: C++
+ */
 class Solution {
-    public:
-        vector<int> pivotArray(vector<int>& nums, int pivot) {
-            vector<int> smaller, larger, ans;
-            int cnt = 0;
-            for(auto ele: nums) {
-                if(ele>pivot) larger.push_back(ele);
-                else if(ele < pivot) smaller.push_back(ele);
-                else cnt++;
-            }
-            ans = smaller;
-            while(cnt--)ans.push_back(pivot);
-            for(auto ele: larger)ans.push_back(ele);
-            return ans;
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> smaller, larger, ans;
+        int cnt = 0;
+        for(auto ele: nums) {
+            if(ele>pivot) larger.push_back(ele);
+            else if(ele < pivot) smaller.push_back(ele);
+            else cnt++;
         }
-    };
+        ans = smaller;
+        while(cnt--)ans.push_back(pivot);
+        for(auto ele: larger)ans.push_back(ele);
+        return ans;
+    }
+};
