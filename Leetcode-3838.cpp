a878a9fba7ec3910ef899d26bf650d06af27df73
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-06-13 19:11:02 +0530

    POTD-13 June 2026

---
+++ Leetcode-3838.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 3838: Weighted Word Mapping (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        
+    }
+};
