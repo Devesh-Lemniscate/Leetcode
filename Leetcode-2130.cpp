63b94b566debfea7de496cd7a72b25b22cc8fc41
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-06-14 07:46:54 +0530

    POTD-14 June 2026

---
+++ Leetcode-2130.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem 2130: Maximum Twin Sum of a Linked List (POTD)
+ * Language: C++
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        
+    }
+};
