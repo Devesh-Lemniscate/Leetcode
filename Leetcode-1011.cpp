1be3f80005e04b10b091e0aae0c484312f5a78e7
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-06-15 10:53:33 +0530

    15 June 2026

---
+++ Leetcode-1011.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 1011: Capacity To Ship Packages Within D Days
+ * Language: C++
+ */
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+            
+    }
+};
