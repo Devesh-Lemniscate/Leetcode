4385c7216351da88629afd9619ceac3e220a2820
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-06-15 11:03:02 +0530

    15 June 2026

--- Leetcode-1011.cpp
+++ Leetcode-1011.cpp
@@ -3,8 +3,33 @@
  * Language: C++
  */
 class Solution {
+private:
+    int check(int mid, vector<int>& weights){
+        int count = mid, ans = 0;
+        for(auto weight: weights){
+            if(weight <= count) count -= weight;
+            else{
+                ans++;
+                count = mid;
+                count -= weight;
+            }
+        }
+        return ans;
+    }
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-            
+        int low = INT_MIN, high = 0;
+        high = accumulate(weights.begin(), weights.end(), 0);
+        low = *max_element(weights.begin(), weights.end());
+        int ans = 0;
+        //cout << low << " " << high;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(check(mid, weights) < days){
+                high = mid-1;
+                ans = mid;
+            }else low = mid+1;
+        }
+        return ans;
     }
 };
