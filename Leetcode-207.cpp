4ba3f2fece376055ba5a691d44be0e03845ab669
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-06-15 11:40:12 +0530

    15 June 2026

--- Leetcode-207.cpp
+++ Leetcode-207.cpp
@@ -4,7 +4,28 @@
  */
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
+    bool canFinish(int n, vector<vector<int>>& arr) {
+        vector<vector<int>> adj(n);
+        for(auto it: arr){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> in(n);
+        for(int i = 0; i < n; i++){
+            for(auto it: adj[i]){
+                in[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i = 0; i < n; i++) if(in[i] == 0) q.push(i);
+        vector<int> ans;
+        while(q.size()){
+            int node = q.front(); q.pop();
+            ans.push_back(node);
+            for(auto it: adj[node]){
+                in[it]--;
+                if(in[it] == 0) q.push(it);
+            }
+        }
+        return ans.size() == n;
     }
 };
