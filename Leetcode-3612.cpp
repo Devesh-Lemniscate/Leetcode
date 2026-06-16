a79e81f4b80a7151d45860e775bb90750b7d2520
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-06-16 07:23:52 +0530

    POTD-16 June 2026

--- Leetcode-3612.cpp
+++ Leetcode-3612.cpp
@@ -5,6 +5,13 @@
 class Solution {
 public:
     string processStr(string s) {
-        
+        string res = "";
+        for(auto ch: s){
+            if(ch=='*' && res.size()) res.pop_back();
+            else if(ch=='%' && res.size()) reverse(res.begin(), res.end());
+            else if(ch=='#' && res.size()) res += res;
+            else if(isalpha(ch))res.push_back(ch);
+        }
+        return res;
     }
 };
