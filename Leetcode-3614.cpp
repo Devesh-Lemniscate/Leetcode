c2e07b0b2b8581d9f893fb099cbccbf6aad9b8f0
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-06-17 09:16:55 +0530

    POTD-17 June 2026

--- Leetcode-3614.cpp
+++ Leetcode-3614.cpp
@@ -5,6 +5,30 @@
 class Solution {
 public:
     char processStr(string s, long long k) {
-        
+        long long len = 0;
+        for(auto ch: s){
+            if(ch == '*') len = max(0LL, len-1);
+            else if(ch == '#') len *= 2;
+            else if(ch == '%') continue;
+            else len++; 
+        }
+        if(len <= k) return '.';
+
+        for(int i = s.size()-1; i >= 0; i--){
+            char ch = s[i];
+            if(ch=='*') len++;
+            else if(ch == '#'){
+                len /= 2;
+                k %= len;
+            }else if(ch == '%'){
+                k = len - k - 1;
+            }else{
+                len--;
+                if(k == len) return ch;
+            }
+        }
+
+
+        return '.';
     }
 };
