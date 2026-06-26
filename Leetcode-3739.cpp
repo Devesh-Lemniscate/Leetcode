ca8b823a26915340e6c26a6642f9f3db44278b25
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-06-26 12:23:43 +0530

    POTD-26 June 2026

---
+++ Leetcode-3739.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 3739: Count Subarrays With Majority Element II (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        
+    }
+};
