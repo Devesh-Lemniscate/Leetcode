05461c365822adbb0142d5d002f7c5bcc38fa23a
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-07-04 09:46:09 +0530

    POTD-04 July 2026

---
+++ Leetcode-2492.cpp
@@ -0,0 +1,31 @@
+/*
+ * Problem 2492: Minimum Score of a Path Between Two Cities (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int, int>>> adjacency(n+1);
+        for(auto road: roads){
+            adjacency[road[0]].push_back({road[1], road[2]});
+            adjacency[road[1]].push_back({road[0], road[2]});
+        }
+        queue<int> bfs;
+        unordered_set<int> visited;
+        int mini = INT_MAX;
+        bfs.push(1);
+        visited.insert(1);
+        while(!bfs.empty()){
+            int node = bfs.front();
+            bfs.pop();
+            for(auto [neighbour, score]: adjacency[node]){
+                mini = min(mini, score);
+                if(!visited.count(neighbour)){
+                    visited.insert(neighbour);
+                    bfs.push(neighbour);
+                }
+            }
+        }
+        return mini;
+    }
+};
