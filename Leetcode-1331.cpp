6edfc256e04ed8543f706a70429a95befc60e94d
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-07-12 09:10:52 +0530

    POTD-12 July 2026

--- Leetcode-1331.cpp
+++ Leetcode-1331.cpp
@@ -5,6 +5,16 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        
+        set<int> s(arr.begin(), arr.end());
+        unordered_map<int, int> m;
+        int pos = 1;
+        for(auto ele: s){
+            m[ele] = pos;
+            pos++;
+        }
+        for(auto& ele: arr){
+            ele = m[ele];
+        }
+        return arr;
     }
 };
