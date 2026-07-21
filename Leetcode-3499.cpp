85355ef969c19e48334d4d877749187723ef275f
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-07-21 13:16:17 +0530

    POTD-21 July 2026

--- Leetcode-3499.cpp
+++ Leetcode-3499.cpp
@@ -9,25 +9,24 @@ public:
         int n = s.size();
         if(n==1) return s[0] == '1';
         int sign = (s[0]=='1') ? 1 : -1;
+        int ans = 0;
         int count = sign;
         for(int i = 1; i < n; i++){
             if(s[i] == s[i-1]) count += sign;
             else{
-                nums.push_back(count);
+                if(count < 0)nums.push_back(count);
+                else ans += count;
                 sign = (sign == 1) ? -1 : 1;
                 count = sign;
             }
         }
-        nums.push_back(count);
-        int ans = max(0, *max_element(nums.begin(), nums.end()));
-        for(int i = 1; i < nums.size()-1; i++){
-            if(nums[i-1] < 0 && nums[i+1] < 0){
-                int temp = 0;
-                if(i-2 >= 0) temp += nums[i-2];
-                if(i+2 < nums.size()) temp += nums[i+2];
-                ans = max(ans, abs(nums[i-1]) + abs(nums[i+1]) + nums[i] + temp); 
-            }
+        if(count < 0)nums.push_back(count);
+        else ans += count;
+        int maxi = 0;
+        for(int i  = 1; i < nums.size(); i++){
+            if(abs(nums[i] + nums[i-1]) > maxi) maxi = abs(nums[i] + nums[i-1]);
         }
-        return ans;
+        
+        return ans + maxi;
     }
 };
