59dbcf663a92b6872c0d130715575a04bae4a65b
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-07-24 07:59:09 +0530

    POTD-24 July 2026

---
+++ Leetcode-3514.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem 3514: Number of Unique XOR Triplets II (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        return n <= 2: n
+    }
+};
