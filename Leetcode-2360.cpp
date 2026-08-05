5065b10583788ef9e3f311b42f6722013d900ea4
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-08-05 08:18:22 +0530

    05 August 2026

--- Leetcode-2360.cpp
+++ Leetcode-2360.cpp
@@ -3,28 +3,29 @@
  * Language: C++
  */
 class Solution {
-private:
-    void dfs(int u, int curr, vector<int>&edge, vector<int>&dis, vector<int>&vis, int &maxi){
-        vis[u] = 1;
-        dis[u] = curr;
-        int next = edge[u];
-        if(next != -1){
-            if(!vis[next]){
-                dfs(next, curr+1, edge, dis, vis, maxi);
-            }else if(dis[next] != -1){
-                maxi = max(maxi, curr-dis[next]+1);
+    void dfs(int node, int currentCount, vector<int>& visited, vector<int> &distance, vector<int> &edges, int &largestCycle){
+        visited[node] = 1;
+        int nextNode = edges[node];
+        distance[node] = currentCount;
+        if(nextNode != -1){
+            if(!visited[nextNode]){
+                dfs(nextNode, currentCount + 1, visited, distance, edges, largestCycle);
+            }else{
+                largestCycle = max(largestCycle, currentCount - distance[nextNode] + 1);
             }
         }
-        dis[u] = -1;
+        distance[node] = -1;
     }
 public:
     int longestCycle(vector<int>& edges) {
-        int n = edges.size();
-        vector<int> dis(n, -1), vis(n, 0);
-        int maxi = -1;
-        for(int i = 0; i < n; i++){
-            if(!vis[i])dfs(i, 1, edges, dis, vis, maxi);
+        int totalNodes = edges.size();
+        int largestCycle = -1;
+        vector<int> visited(totalNodes, 0), distance(totalNodes, -1);
+        for(int node = 0; node < totalNodes; node++){
+            if(!visited[node]){
+                dfs(node, 1, visited, distance, edges, largestCycle);
+            }
         }
-        return maxi;
+        return largestCycle;
     }
 };
