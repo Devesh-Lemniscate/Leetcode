c2c4648824ac871ad436cdb1d2e9aeabdac2bc84
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-08-05 08:43:12 +0530

    05 August 2026

---
+++ Leetcode-2359.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 2359: Find Closest Node to Given Two Nodes
+ * Language: C++
+ */
+class Solution {
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        
+    }
+};
