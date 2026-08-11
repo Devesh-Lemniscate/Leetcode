9e03403f85ed2841934f0c2e86e3b775a01945e3
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-08-11 08:54:35 +0530

    11 August 2026

--- Leetcode-138.cpp
+++ Leetcode-138.cpp
@@ -21,23 +21,56 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> mp;
-        Node* temp = head, *dummy = new Node(-1);
-        Node* newHead = dummy;
+        Node *temp = head;
+        Node *dummy = new Node(10);
+        Node *temp1 = dummy;
+        // step 1 : deep copy without random pointer
         while(temp){
-            dummy->next = new Node(temp->val);
-            dummy = dummy->next;
-            mp[temp] = dummy;
+            Node *a = new Node(temp->val);
+            temp1->next = a;
             temp = temp->next;
+            temp1 = temp1->next;
         }
-        temp = head;
-        dummy = newHead->next;
-        newHead = newHead->next;
-        while(temp){
-            newHead->random = mp[temp->random];
-            temp = temp->next;
-            newHead = newHead->next;
+        // step 2 : create alternate connections 
+        Node *dupli = dummy->next;
+        Node *a = head, *b=dupli;
+        dummy = new Node(-1);
+        Node *tempD = dummy;
+        while(a){
+            tempD->next = a;
+            a =a->next;
+            tempD = tempD->next;
+            tempD->next = b;
+            b =b->next;
+            tempD = tempD->next;
         }
-        return dummy;
-    }   
+        dummy = dummy->next;
+        // step 3 : make alternate connections
+        Node *t1 = dummy, *t2;
+        while(t1){
+            t2 = t1->next;
+            if(t1->random) t2->random = t1->random->next;
+            t1 = t1->next->next;
+        }
+        // step 4 : seperate connections
+        Node * d1 = new Node(-1);
+        Node * d2 = new Node(-1);
+        t1=d1;
+        t2=d2;
+        Node *t = dummy;
+        while(t){
+            t1->next = t;
+            t = t->next;
+            t1 = t1->next;
+            t2->next = t;
+            t = t->next;
+            t2 = t2->next;
+        }
+        t1->next = NULL;
+        t2->next = NULL;
+        d1 = d1->next;
+        d2 = d2->next;
+        return d2;
+
+    }
 };
