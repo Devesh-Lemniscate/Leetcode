68228b42fc4c72bea57fc102d759247093405598
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-08-14 16:31:20 +0530

    14 August 2026

--- Leetcode-236.cpp
+++ Leetcode-236.cpp
@@ -14,6 +14,13 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || root == p || root == q) return root;
+
+        TreeNode* leftTree = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightTree = lowestCommonAncestor(root->right, p, q);
         
+        if(leftTree && rightTree) return root; 
+        if(!leftTree) return rightTree;
+        else return leftTree;
     }
 };
