2361061fc845b2d0b145a9cc9ee9e05617246151
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-08-16 20:06:48 +0530

    16 August 2026

--- Leetcode-424.cpp
+++ Leetcode-424.cpp
@@ -1,20 +1,13 @@
+/*
+ * Problem 424: Longest Repeating Character Replacement
+ * Language: C++
+ */
 class Solution {
-    public:
-        int characterReplacement(string s, int k) {
-            int i = 0, j = 0, maxLen = 0, maxFreq = 0;
-            int arr[26] = {0};
-            while(j < s.size()){
-                arr[s[j]-'A']++;
-                maxFreq = max(maxFreq, arr[s[j]-'A']);
-                while(j-i+1-maxFreq > k){
-                    arr[s[i]-'A']--;
-                    maxFreq = 0;
-                    for(int i = 0; i < 26; i++) maxFreq = max(maxFreq, arr[i]);
-                    i++;
-                }
-                if(j-i+1-maxFreq <= k) maxLen = max(maxLen, j-i+1);
-                j++;
-            }
-            return maxLen;
+public:
+    int characterReplacement(string s, int k) {
+        int left = 0, right = 0, len = s.size();
+        while(right < len && k){
+            if(s[right++] )
         }
-    };
+    }
+};
