58b72855a4a865bf3dcd7464387d5dd475ff9d66
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-08-20 07:49:34 +0530

    POTD-20 August 2026

---
+++ Leetcode-3069.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem 3069: Distribute Elements Into Two Arrays I (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> nums1, nums2, ans;
+        nums1.push_back(nums[0]);
+        nums2.push_back(nums[1]);
+        for(int i = 2; i < nums.size(); i++){
+            if(nums1.back() > nums2.back()) nums1.push_back(nums[i]);
+            else nums2.push_back(nums[i]);
+        }
+        for(auto num: nums1) ans.push_back(num);
+        for(auto num: nums2) ans.push_back(num);
+        return ans;
+    }
+};
