c7db0a662679e089ee596114f506d0d169dbd2a1
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-08-30 14:07:44 +0530

    30 August 2026

--- Leetcode-57.cpp
+++ Leetcode-57.cpp
@@ -4,22 +4,23 @@
  */
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(intervals.begin(), intervals.end());
-        int start, end;
-        for(int ind = 0; ind < intervals.size();){
-            start = intervals[ind][0], end = intervals[ind][1];
-            
-                while(ind < intervals.size() && end <= nums[1] && nums[0] <= end && nums[0] >= start){
-                    start = min(start, nums[0]);
-                    end = max(end, nums[1]);
-                    ind++;
-                }
-            ind++;
-            ans.push_back({start, end});
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> result;
+        int n = intervals.size();
+        int index = 0;
+        int newStart = newInterval[0], newEnd = newInterval[1];
+
+        while(index < n && intervals[index][1] < newStart) result.push_back(intervals[index++]);
+
+        while(index < n && intervals[index][0] <= newEnd) {
+            newStart = min(newStart, intervals[index][0]);
+            newEnd = max(newEnd, intervals[index][1]);
+            index++;
         }
+        result.push_back({newStart, newEnd});
+
+        while(index < n) result.push_back(intervals[index++]);
         
-        return ans;
+        return result;
     }
 };
