2d831d9e0280053ff560b083f5081bd50ffa4e78
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-08-31 09:13:21 +0530

    31 August 2026

--- Leetcode-329.cpp
+++ Leetcode-329.cpp
@@ -5,15 +5,14 @@
 class Solution {
 private:
     int n, m;
-    int dp[201][201][5];
+    int dp[201][201];
     int dirX[4] = {0, 0, 1, -1};
     int dirY[4] = {1, -1, 0, 0};
-    int helper(int i, int j, int dir, vector<vector<int>> &nums){
+    int helper(int i, int j, vector<vector<int>> &nums){
         if(i < 0 || j < 0 || i == n || j == m) return 0;
         if(dp[i][j][dir] != -1) return dp[i][j][dir];
         int ans = 1;
         for(int k = 0; k < 4; k++){
-            if((dir == 0 && k == 1) || (dir == 1 && k == 0) || (dir == 2 && k == 3) || (dir == 3 && k ==2)) continue;
             int ni = i + dirX[k], nj = j + dirY[k];
             if(ni >= 0 && ni < n && nj >= 0 && nj < m && nums[ni][nj] > nums[i][j]){
                 ans = max(ans, 1 + helper(ni, nj, k, nums));
