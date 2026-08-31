e3bc678354d73d2ec527b24014685a8b555e95fc
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-08-31 07:19:15 +0530

    POTD-31 August 2026

---
+++ Leetcode-2058.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem 2058: Find the Minimum and Maximum Number of Nodes Between Critical Points (POTD)
+ * Language: C++
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        
+    }
+};
