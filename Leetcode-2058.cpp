b92203c364430931eb6d21f25b50d3b5456ff6ef
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-08-31 07:33:54 +0530

    POTD-31 August 2026

--- Leetcode-2058.cpp
+++ Leetcode-2058.cpp
@@ -15,6 +15,30 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        
+        ListNode* current = head;
+        if(!head || !head->next || !head->next->next) return {-1, -1};
+        ListNode* prev = head;
+        current = current->next;
+        int index = 0, firstCritical = -1, prevCritical = -1;
+        int minDistance = INT_MAX, maxDistance = INT_MIN;
+        while(current->next){
+            if((prev->val < current->val && current->next->val < current->val) || (prev->val > current->val && current->next->val > current->val)){
+                if(firstCritical == -1){
+                    firstCritical = index;
+                    prevCritical = index;
+                }
+                else{
+                    minDistance = min(minDistance, index - prevCritical);
+                    maxDistance = index - firstCritical;
+                    prevCritical = index;
+                }
+            }
+            prev = current;
+            current = current->next;
+            index++;
+        }
+
+        if(maxDistance == INT_MIN || minDistance == INT_MAX) return {-1, -1};
+        else return {minDistance, maxDistance};
     }
 };
