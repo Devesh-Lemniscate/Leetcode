41bdd5e991670c2b06d805dc389f37ed9355ee7c
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-08-31 09:13:51 +0530

    31 August 2026

--- Leetcode-329.cpp
+++ Leetcode-329.cpp
@@ -10,15 +10,15 @@ private:
     int dirY[4] = {1, -1, 0, 0};
     int helper(int i, int j, vector<vector<int>> &nums){
         if(i < 0 || j < 0 || i == n || j == m) return 0;
-        if(dp[i][j][dir] != -1) return dp[i][j][dir];
+        if(dp[i][j] != -1) return dp[i][j];
         int ans = 1;
         for(int k = 0; k < 4; k++){
             int ni = i + dirX[k], nj = j + dirY[k];
             if(ni >= 0 && ni < n && nj >= 0 && nj < m && nums[ni][nj] > nums[i][j]){
-                ans = max(ans, 1 + helper(ni, nj, k, nums));
+                ans = max(ans, 1 + helper(ni, nj, nums));
             }
         }
-        return dp[i][j][dir] = ans;
+        return dp[i][j] = ans;
     }
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
@@ -27,7 +27,7 @@ public:
         memset(dp, -1, sizeof(dp));
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                maxi = max(maxi, helper(i, j, 4, matrix));
+                maxi = max(maxi, helper(i, j, matrix));
             }
         }
         return maxi;
