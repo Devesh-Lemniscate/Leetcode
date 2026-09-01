2b1b052570d54dde00c4cde5b37c1b312bf10eac
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-09-01 07:22:10 +0530

    POTD-01 September 2026

---
+++ Leetcode-3568.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 3568: Minimum Moves to Clean the Classroom (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    int minMoves(vector<string>& classroom, int energy) {
+        
+    }
+};
