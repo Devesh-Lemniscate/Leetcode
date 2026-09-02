2f3d853c54acf818aa327b4db43681b89d36f472
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-09-02 07:36:55 +0530

    POTD-02 September 2026

---
+++ Leetcode-3875.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem 3875: Construct Uniform Parity Array I (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
