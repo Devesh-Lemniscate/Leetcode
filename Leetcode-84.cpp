b3d6c5c25e16b0830bc535be2f5e0fafff5c0b3f
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-09-06 17:57:07 +0530

    06 September 2026

--- Leetcode-84.cpp
+++ Leetcode-84.cpp
@@ -10,9 +10,26 @@ public:
         stack<int> st;
         st.push(n);
         for(int i = n-2; i >= 0; i--){
-            while(!st.empty() && heights[st.top()] ){
-
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                st.pop();
             }
+            if(st.empty()) nextSmaller[i] = 
+            else nextSmaller[i] = st.top();
         }
+        while(!st.empty()) st.pop();
+
+        for(int i = 1; i < n; i++){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                st.pop();
+            }
+            if(st.empty())
+            else prevSmaller[i] = st.top();
+        }
+
+        int area = 0;
+        for(int i = 0; i < n; i++){
+            area = max(area, heights[i] * (nextSmaller[i] - prevSmaller[i]));
+        }
+        return area;
     }
 };
