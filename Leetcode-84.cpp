2f55953e680d912d4f3a963f9ae2cfb4a2e23e2f
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-09-06 17:51:51 +0530

    06 September 2026

--- Leetcode-84.cpp
+++ Leetcode-84.cpp
@@ -5,6 +5,14 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        
+        int n = heights.size();
+        vector<int> nextSmaller(n), prevSmaller(n);
+        stack<int> st;
+        st.push(n);
+        for(int i = n-2; i >= 0; i--){
+            while(!st.empty() && heights[st.top()] ){
+
+            }
+        }
     }
 };
