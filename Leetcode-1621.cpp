20b9ff652e15800b4edd40280130f04c804edebe
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-09-16 08:19:25 +0530

    POTD-16 September 2026

---
+++ Leetcode-1621.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem 1621: Number of Sets of K Non-Overlapping Line Segments (POTD)
+ * Language: C++
+ */
+class Solution {
+private:
+    const int mod = 1e9+7;
+    int n, k;
+    int dp[1001][1001][2];
+    int helper(int ind, int count, bool flag){
+        if(ind >= n) return 0;
+        if(count == k) return 1;
+        if(dp[ind][count][flag] != -1) return dp[ind][count][flag];
+        int ans = 0;
+        if(flag) ans = (ans % mod + helper(ind, (count+1) % mod, !flag) % mod) % mod;
+        else ans = (ans % mod + helper(ind+1, count, !flag) % mod) % mod;
+
+        return dp[ind][count][flag] = (ans%mod + helper(ind+1, count, flag) % mod) % mod;
+    }
+public:
+    int numberOfSets(int num, int val) {
+        n = num, k = val;
+        int ans = 0;
+        memset(dp, -1, sizeof(dp));
+        ans += helper(0, 0, false);
+        return ans;
+    }
+};
