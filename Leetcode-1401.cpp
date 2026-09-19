3359a1c1e6c2f12f246d4f3926c3e4317808b279
Dave The Lemniscate <139860394+Devesh-Lemniscate@users.noreply.github.com>
2026-09-19 07:13:26 +0530

    POTD-19 September 2026

---
+++ Leetcode-1401.cpp
@@ -0,0 +1,12 @@
+/*
+ * Problem 1401: Circle and Rectangle Overlapping (POTD)
+ * Language: C++
+ */
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int x = max(x1, min(x2, xCenter)) - xCenter;
+        int y = max(y1, min(y2, yCenter)) - yCenter;
+        return x*x + y*y <= radius * radius;
+    }
+};
